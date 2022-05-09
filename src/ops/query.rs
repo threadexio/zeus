5a9b7248a684d987774f9987298afb533c357c20
threadexio <pzarganitis@gmail.com>
2022-05-09 16:05:19 +0300

    fix null fields on aur queries

--- src/ops/query.rs
+++ src/ops/query.rs
@@ -12,6 +12,17 @@ macro_rules! print_if_some {
 		match $b {
 			None => {}
 			Some(v) => {
+				println!("{0: <16}: {1}", $a, v);
+			}
+		}
+	}};
+}
+
+macro_rules! print_vec_if_some {
+	($a:expr,$b:expr) => {{
+		match $b {
+			None => {}
+			Some(v) => {
 				println!("{0: <16}: {1}", $a, v.join(" "));
 			}
 		}
@@ -19,42 +30,36 @@ macro_rules! print_if_some {
 }
 
 fn print_pretty_package(package: &aur::Package) {
-	println!("{0: <16}: {1}", "Name", &package.Name);
-	println!("{0: <16}: {1}", "Version", &package.Version);
-	println!("{0: <16}: {1}", "Description", &package.Description);
-	println!("{0: <16}: {1}", "URL", &package.URL);
+	print_if_some!("Name", &package.Name);
+	print_if_some!("Version", &package.Version);
+	print_if_some!("Description", &package.Description);
+	print_if_some!("URL", &package.URL);
 
-	print_if_some!("License", &package.License);
-	print_if_some!("Groups", &package.Groups);
-	print_if_some!("Provides", &package.Provides);
-	print_if_some!("Depends On", &package.Depends);
-	print_if_some!("Optional Deps", &package.OptDepends);
-	print_if_some!("Conflicts", &package.Conflicts);
-	print_if_some!("Replaces", &package.Replaces);
+	print_vec_if_some!("License", &package.License);
+	print_vec_if_some!("Groups", &package.Groups);
+	print_vec_if_some!("Provides", &package.Provides);
+	print_vec_if_some!("Depends On", &package.Depends);
+	print_vec_if_some!("Optional Deps", &package.OptDepends);
+	print_vec_if_some!("Conflicts", &package.Conflicts);
+	print_vec_if_some!("Replaces", &package.Replaces);
 
 	println!(
 		"{0: <16}: {1}",
 		"Maintainer",
-		match &package.Maintainer {
-			Some(v) => v,
-			None => "none",
-		}
+		&package.Maintainer.as_ref().unwrap_or(&"none".to_owned())
 	);
 
-	println!("{0: <16}: {1}", "Last Modified", &package.LastModified);
-	println!("{0: <16}: {1}", "First Submitted", &package.FirstSubmitted);
+	print_if_some!("Last Modified", &package.LastModified);
+	print_if_some!("First Submitted", &package.FirstSubmitted);
 
 	println!(
 		"{0: <16}: {1}",
 		"Out of date",
-		match package.OutOfDate {
-			Some(_) => "yes",
-			None => "no",
-		}
+		&package.OutOfDate.unwrap_or(0)
 	);
 
-	println!("{0: <16}: {1}", "Popularity", package.Popularity);
-	println!("{0: <16}: {1}", "Votes", package.NumVotes);
+	print_if_some!("Popularity", &package.Popularity);
+	print_if_some!("Votes", &package.NumVotes);
 }
 
 pub async fn query(
@@ -91,7 +96,12 @@ pub async fn query(
 						Level::Info,
 						format!(
 							"{} - {}\n\t{}",
-							package.Name, package.Version, package.Description,
+							package.Name.as_ref().unwrap_or(&"unnamed".to_owned()),
+							package.Version.as_ref().unwrap_or(&"0.0.0-0".to_owned()),
+							package
+								.Description
+								.as_ref()
+								.unwrap_or(&"No description".to_owned()),
 						),
 					);
 				}
