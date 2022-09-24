36bc63902850a19e782d429e1875765e735b1bae
threadexio <pzarganitis@gmail.com>
2022-09-24 14:11:03 +0300

    organise build configs
    Signed-off-by: threadexio <pzarganitis@gmail.com>

--- build.config.debug.mk
+++ build.config.debug.mk
@@ -1,15 +1,12 @@
 export BUILD_PROFILE := debug
-
-export PREFIX := /usr/local
-
-export BUILDER_NAME := zeus-builder
+export LOG_LEVEL	 := info
+export RUNTIME		 := docker
+export BUILDER_NAME	 := zeus-builder
 export BUILDER_IMAGE := zeus-builder
-export BUILD_DIR := /var/cache/aur
-export AUR_URL := https://aur.archlinux.org/
-export RUNTIME := docker
-export RUNTIME_DIR := $(PREFIX)/lib/zeus/runtimes
-export DATA_DIR := $(PREFIX)/share/zeus
-export LOG_LEVEL := info
-
-export VERSION	:= $(shell scripts/version.sh)
-export BUILD_INFO := $(shell scripts/build_info.sh)
+export AUR_URL		 := https://aur.archlinux.org/
+export PREFIX		 := /usr/local
+export BUILD_DIR	 := /var/cache/aur
+export RUNTIME_DIR	 := $(PREFIX)/lib/zeus/runtimes
+export DATA_DIR		 := $(PREFIX)/share/zeus
+export VERSION		 := $(shell scripts/version.sh)
+export BUILD_INFO	 := $(shell scripts/build_info.sh)
