d8f92349ea5f785b9d04fdee91443564d0b6c0f2
threadexio <pzarganitis@gmail.com>
2023-02-13 20:10:32 +0200

    feat: add custom runtime
    Signed-off-by: threadexio <pzarganitis@gmail.com>

---
+++ runtimes/zeus_rt_custom/src/lib.rs
@@ -0,0 +1,95 @@
+use std::env;
+use std::path::{Path, PathBuf};
+use std::process::{self, Stdio};
+
+use zeus::{log::macros::*, runtime::*};
+
+struct CustomRuntime {
+	script_dir: PathBuf,
+}
+
+impl CustomRuntime {
+	fn script(&self, script: &str) -> process::Command {
+		let mut cmd =
+			process::Command::new(self.script_dir.join(script));
+		cmd.stdin(Stdio::inherit())
+			.stdout(Stdio::inherit())
+			.stderr(Stdio::inherit());
+		cmd
+	}
+
+	fn run_script(&self, mut cmd: process::Command) -> Result<()> {
+		let output = match cmd.output() {
+			Ok(v) => v,
+			Err(e) => {
+				return Err(Error::new(format!(
+					"unable to run script `{}`: {e}",
+					cmd.get_program().to_string_lossy()
+				)))
+			},
+		};
+
+		if !output.status.success() {
+			return Err(Error::new(format!(
+				"script exited with: {}",
+				output.status.code().unwrap_or(-1)
+			)));
+		}
+
+		Ok(())
+	}
+}
+
+impl Default for CustomRuntime {
+	fn default() -> Self {
+		Self { script_dir: Path::new("scripts").to_path_buf() }
+	}
+}
+
+impl IRuntime for CustomRuntime {
+	fn name(&self) -> &'static str {
+		"custom"
+	}
+
+	fn version(&self) -> &'static str {
+		env!("CARGO_PKG_VERSION", "must be built with cargo")
+	}
+
+	fn init(&mut self, config: &GlobalConfig) -> Result<()> {
+		set_log_level!(config.log_level);
+
+		let opts = &config.runtime_opts;
+
+		if let Some(v) = env::var_os("SCRIPT_DIR")
+			.map(|x| Path::new(&x).to_path_buf())
+			.or(opts
+				.get("ScriptDir")
+				.map(|x| Path::new(&x).to_path_buf()))
+		{
+			self.script_dir = v;
+		}
+
+		self.run_script(self.script("init"))?;
+
+		Ok(())
+	}
+
+	fn exit(&mut self) {}
+
+	fn create_image(&mut self, _: &GlobalConfig) -> Result<()> {
+		self.run_script(self.script("create_image"))?;
+		Ok(())
+	}
+
+	fn create_machine(&mut self, _: &GlobalConfig) -> Result<()> {
+		self.run_script(self.script("create_machine"))?;
+		Ok(())
+	}
+
+	fn start_machine(&mut self, _: &GlobalConfig) -> Result<()> {
+		self.run_script(self.script("create_machine"))?;
+		Ok(())
+	}
+}
+
+runtime!(CustomRuntime::default);
